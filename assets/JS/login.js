f0dd9a8275acc318d85c81b9611adb7b1e248ff0
Leonardolimacavalcante890 <leonardolimacavalcante8@gmail.com>
2024-06-20 21:17:05 -0300

    tentando colocar os zoi

--- assets/JS/login.js
+++ assets/JS/login.js
@@ -1,6 +1,30 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const password = document.getElementsByClassName('password');
+const icon1 = document.getElementsById('icon1');
+const icon2 = document.getElementsById('icon2');
+
+function showHide(){
+    if(password.type === "password"){
+        password.setAttribute('type' , 'text');
+        icon1.classList.add('hide');
+    }
+    else{
+        password.setAttribute('type' , 'password');
+        icon1.classList.remove('hide')
+    }
+}
+function showHide2(){
+    if(password.type === "password"){
+        password.setAttribute('type' , 'text');
+        icon2.classList.add('hide');
+    }
+    else{
+        password.setAttribute('type' , 'password');
+        icon2.classList.remove('hide')
+    }
+}
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -9,3 +33,5 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
