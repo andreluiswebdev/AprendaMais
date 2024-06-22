588008322eef52f5e3e2592814bc3027d82c2d95
André Luis <andreluis.h@hotmail.com>
2024-06-22 14:04:48 -0300

    Resolvendo conflito em index.html

--- assets/JS/teste.js
+++
@@ -1,9 +0,0 @@
-const d20 = document.getElementById("d20");
-
-d20.addEventListener("click", () => {
-  if (d20.style.animationPlayState === "paused") {
-    d20.style.animationPlayState = "running";
-  } else {
-    d20.style.animationPlayState = "paused";
-  }
-});
