eb409bcbcb712c8d0ee85ee90efc97c783a1166a
André Luis <andreluis.h@hotmail.com>
2024-06-22 21:16:33 -0300

    finalizando a home

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,22 +1,17 @@
-const d20 = document.getElementById("d20");
 
-d20.addEventListener("click", () => {
-  if (d20.style.animationPlayState === "paused") {
-    d20.style.animationPlayState = "running";
-  } else {
-    d20.style.animationPlayState = "paused";
+// revelar página com o scroll
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.hidden');
+
+  for(var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 200;
+
+    if(revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('reveal');
+    }
   }
-});
-
-
-const trofeuImg = document.getElementById("trofeuimg");
-
-// Iniciar a animação (por exemplo, quando a página carrega)
-window.addEventListener("load", () => {
-  trofeuImg.style.animationPlayState = "running";
-});
-
-// Parar a animação (por exemplo, ao clicar no troféu)
-trofeuImg.addEventListener("click", () => {
-  trofeuImg.style.animationPlayState = "paused";
-});
+}
