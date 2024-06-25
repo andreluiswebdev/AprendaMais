8f040769bc9c8d52bdd589d675c54cebc84fa7ca
GabrielKameoka <gabrielkameoka@gmail.com>
2024-06-25 19:50:02 -0300

    add header/footer

--- quizApp/script.js
+++ quizApp/script.js
@@ -9,3 +9,16 @@ function selecionarAssunto(evento) {
     console.log(evento);
     window.location.href = "./quizz/exercicio.html"
 }
+
+// INICIO SIDEBAR
+document.getElementById('open_btn').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    const icon = document.getElementById('open_btn_icon');
+
+    sidebar.classList.toggle('open-sidebar');
+
+    // Alterna apenas entre bi-chevron-right e bi-chevron-left
+    icon.classList.toggle('bi-chevron-right');
+    icon.classList.toggle('bi-chevron-left'); 
+});
+// FIM SIDEBAR
