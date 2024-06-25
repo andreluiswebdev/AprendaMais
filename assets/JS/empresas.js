b6e7fc028e35cdce271e0a467cb58f45892dd699
André Luis <andreluis.h@hotmail.com>
2024-06-25 00:43:11 -0300

    finalizando pagina para empresas

--- assets/JS/empresas.js
+++ assets/JS/empresas.js
@@ -1,3 +1,5 @@
+
+// INICIO TABELAS
 var tabs = document.getElementsByClassName('tab-item');
 var contents = document.getElementsByClassName('tab-pane');
 
@@ -10,4 +12,39 @@ for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.add('active');
     contents[i].classList.add('active');
   });
+}
+// FIM TABELAS
+
+
+// INICIO FORM
+function validatePhone(input) {
+  var regex = /^[+()-.x0-9]*$/;
+  var errorMsg = document.getElementById('phoneError');
+  if (!regex.test(input.value)) {
+    errorMsg.textContent = "Um número de telefone válido pode conter apenas números, +()-. ou x";
+    setTimeout(function() {
+      errorMsg.textContent = "";
+    }, 3000); // Faz a mensagem desaparecer após 3 segundos
+  } else {
+    errorMsg.textContent = "";
+  }
+}
+// FIM FORM
+
+
+// revelar página com o scroll
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.hidden');
+
+  for(var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 200;
+
+    if(revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('reveal');
+    }
+  }
 }
